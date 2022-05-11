5064ebdf7cfa8d67879ce28e5bd3af81e7dc7e24
onioneggy <zhaoyanliu5670@gmail.com>
2022-05-11 16:00:08 +0800

    some changes

--- dist/app.js
+++ dist/app.js
@@ -9,5 +9,7 @@ const routes_1 = __importDefault(require("./routes/routes"));
 const app = (0, express_1.default)();
 app.use((0, body_parser_1.json)());
 app.use('/employee', routes_1.default);
-app.use((request, response) => { });
+app.use((err, request, response, next) => {
+    response.status(404).json({ message: err.message });
+});
 app.listen(3000);
--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
-import express from 'express'
+import express, {Request, Response, NextFunction} from 'express'
 import { json } from 'body-parser'
 import routes from "./routes/routes"
 
 const app = express()
 app.use(json())
 app.use('/employee', routes)
-app.use((request,response) => {})
+app.use((err: Error, request: Request,response: Response, next: NextFunction) => {
+    response.status(404).json({message: err.message})
+})
 app.listen(3000)
