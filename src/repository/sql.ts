7b241d2ea129442cbdd764b826e158cb654f3cbc
onioneggy <zhaoyanliu5670@gmail.com>
2022-05-23 11:34:47 +0800

    removed console messages

--- src/repository/sql.ts
+++ src/repository/sql.ts
@@ -3,22 +3,18 @@ import { Employee } from "../models";
 import { Pool } from "pg"
 
 export class SqlEmployeeRepository implements EmployeeRepository {
-    // private idTag: number = 0
     constructor(private pool: Pool) {}
 
     async findAllEmployees(): Promise<Employee[]> {
         const employees = await this.pool.query(`select * from employee`)
         // console.log(employees)
         const result = employees.rows.map(employee => new Employee(employee.id, employee.name, employee.salary, employee.department))
-        console.log(result)
         return result
     }
 
     async findEmployeeWithId(id: number): Promise<Employee | undefined> {
         const employeeWithId = await this.pool.query(`select * from employee where id=$1`, [id])
-        console.log(employeeWithId)
         const result = employeeWithId.rows.map(employee => new Employee(employee.id, employee.name, employee.salary, employee.department))
-        console.log(result[0])
         return result[0]
     }
     
@@ -29,13 +25,10 @@ export class SqlEmployeeRepository implements EmployeeRepository {
 
     async addNewEmployee(request: CreateEmployeeRequest): Promise<Employee> {
         const query = `insert into employee values(default, $1, $2, $3) returning id`
-        console.log(query)
         const id = (await this.pool.query(query, [request.name, request.salary, request.department])).rows[0].id
-        console.log(id)
 
         const newEmployee = await this.pool.query(`select * from employee where id=$1`, [id])
         const result = newEmployee.rows.map(employee => new Employee(id, employee.name, employee.salary, employee.department))
-        console.log(result[0])
         return result[0]
     }
 
@@ -43,7 +36,6 @@ export class SqlEmployeeRepository implements EmployeeRepository {
         const employeeToBeUpdated = await this.findEmployeeWithId(employee.id)
         if (employeeToBeUpdated) {
             const query = `update employee set name=$1, salary=$2, department=$3 where id=$4`
-            console.log(query)
             await this.pool.query(query, [employee.name, employee.salary, employee.department, employee.id])
             
         }
@@ -51,9 +43,3 @@ export class SqlEmployeeRepository implements EmployeeRepository {
         return employeeToBeUpdated
     }
 }
-
-// create table employee (
-// id int,
-// name varchar(2),
-// salary int,
-// department varchar(2));
